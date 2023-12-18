671031b5f05fa60264b1e41bafdc74c2ba6fab7d
Charlie Curnin <charliecurnin@me.com>
2023-12-18 14:06:19 -0800

    simplify ui

--- webui/webui/components/navbar.py
+++ webui/webui/components/navbar.py
@@ -8,52 +8,11 @@ def navbar():
     return rx.box(
         rx.hstack(
             rx.hstack(
-                rx.icon(
-                    tag="hamburger",
-                    mr=4,
-                    on_click=State.toggle_drawer,
-                    cursor="pointer",
-                ),
-                rx.link(
-                    rx.box(
-                        rx.image(src="favicon.ico", width=30, height="auto"),
-                        p="1",
-                        border_radius="6",
-                        bg="#F0F0F0",
-                        mr="2",
-                    ),
-                    href="/",
-                ),
                 rx.breadcrumb(
                     rx.breadcrumb_item(
-                        rx.heading("ReflexGPT", size="sm"),
-                    ),
-                    rx.breadcrumb_item(
-                        rx.text(State.current_chat, size="sm", font_weight="normal"),
-                    ),
+                        rx.heading("Venture Chatitalist", size="sm"),
+                    )
                 ),
-            ),
-            rx.hstack(
-                rx.button(
-                    "+ New chat",
-                    bg=styles.accent_color,
-                    px="4",
-                    py="2",
-                    h="auto",
-                    on_click=State.toggle_modal,
-                ),
-                rx.menu(
-                    rx.menu_button(
-                        rx.avatar(name="User", size="md"),
-                        rx.box(),
-                    ),
-                    rx.menu_list(
-                        rx.menu_item("Help"),
-                        rx.menu_divider(),
-                        rx.menu_item("Settings"),
-                    ),
-                ),
-                spacing="8",
             ),
             justify="space-between",
         ),
